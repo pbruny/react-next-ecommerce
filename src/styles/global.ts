05e9a639845ac4e44d3e4ed6898d2486bb9314ba
Paulo Bruny Lima <paulobruny@gmail.com>
2020-11-22 21:06:25 -0300

    set font-display and antialiasing to fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
+    font-display: swap;
     src: local(''),
         url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
@@ -13,6 +14,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
+    font-display: swap;
     src: local(''),
         url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
 
@@ -22,6 +24,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
+    font-display: swap;
     src: local(''),
         url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
@@ -30,6 +33,8 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
